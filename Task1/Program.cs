50615e3b74bbe8accd0c5fbe56d1e206ccde176b
Marat <ashrapovm@yandex.ru>
2023-07-13 22:13:31 +0400

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfTurnsPerPlayer = 5;
+            string[] fieldWithX = new string[numberOfTurnsPerPlayer];
+            string[] fieldWithO = new string[numberOfTurnsPerPlayer];
+            string[,] fieldOfPlay = { { "1", "2", "3"}, { "4", "5", "6"}, { "7", "8", "9"} };
+            int movesWereMade = 0;
+            string[] winnigCombination = null;
+
+            while (winnigCombination == null && movesWereMade < 9)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Console.Write(fieldOfPlay[i, j] + "|");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("------");
+                }
+
+                if (movesWereMade % 2 == 0)
+                {
+                    Console.WriteLine("Ход игрока 1 (Крестик): ");
+                    fieldWithX[movesWereMade / 2] = NumberCheck();
+                    if (fieldWithX[movesWereMade / 2] == "Ошибка")
+                    {
+                        Console.WriteLine("Были ведены не корректные данные, попробуйте вести число от 1 до 9");
+                        continue;
+                    }
+                    if (fieldOfPlay[(Convert.ToInt32(fieldWithX[movesWereMade / 2]) - 1) / 3, (Convert.ToInt32(fieldWithX[movesWereMade / 2]) - 1) % 3] != "X" &&
+                       fieldOfPlay[(Convert.ToInt32(fieldWithX[movesWereMade / 2]) - 1) / 3, (Convert.ToInt32(fieldWithX[movesWereMade / 2]) - 1) % 3] != "O")
+                    {
+                        fieldOfPlay[(Convert.ToInt32(fieldWithX[movesWereMade / 2]) - 1) / 3, (Convert.ToInt32(fieldWithX[movesWereMade / 2]) - 1) % 3] = "X";
+                        winnigCombination = WinCheck(fieldWithX);
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Данная ячейка уже заполнена, попробуйте другую.");
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ход игрока 2 (Нолик): ");
+                    fieldWithO[movesWereMade / 2] = NumberCheck();
+                    if (fieldWithO[movesWereMade / 2] == "Ошибка")
+                    {
+                        Console.WriteLine("Были ведены не корректные данные, попробуйте вести число от 1 до 9");
+                        continue;
+                    }
+                    if (fieldOfPlay[(Convert.ToInt32(fieldWithO[movesWereMade / 2]) - 1) / 3, (Convert.ToInt32(fieldWithO[movesWereMade / 2]) - 1) % 3] != "X" &&
+                       fieldOfPlay[(Convert.ToInt32(fieldWithO[movesWereMade / 2]) - 1) / 3, (Convert.ToInt32(fieldWithO[movesWereMade / 2]) - 1) % 3] != "O")
+                    {
+                        fieldOfPlay[(Convert.ToInt32(fieldWithO[movesWereMade / 2]) - 1) / 3, (Convert.ToInt32(fieldWithO[movesWereMade / 2]) - 1) % 3] = "O";
+                        winnigCombination = WinCheck(fieldWithO);
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Данная ячейка уже заполнена, попробуйте другую.");
+                        continue;
+                    }
+                    
+                }
+
+                movesWereMade++;
+
+            }
+
+            if (winnigCombination != null && movesWereMade % 2 == 0 && movesWereMade > 4 && movesWereMade < 9)
+            {
+                Console.WriteLine("Выиграл игрок 2");
+                ShowWinningCombination(fieldOfPlay, fieldWithO);
+            }
+            else if (winnigCombination != null && movesWereMade % 2 == 1 && movesWereMade > 4 && movesWereMade < 9)
+            {
+                Console.WriteLine("Выиграл игрок 1");
+                ShowWinningCombination(fieldOfPlay, fieldWithX);
+            }
+            else
+            {
+                Console.WriteLine("Ничья");
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Console.Write(fieldOfPlay[i, j] + "|");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("------");
+                }
+            }
+
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Проверка выигрыша игрока.
+        /// </summary>
+        /// <param name="field">Поле</param>
+        /// <returns></returns>
+        static string[] WinCheck(string[] field)
+        {
+            List<string> winnigCombination = new List<string>(3);
+            int numberOfTurnsPerPlayer = 5;
+            int numberOfIdenticalCharactera = 0;
+            int maxNumberOfIdenticalCharactera = 0;
+            // Проверка вертикальных линий
+            for (int i = 0; i < numberOfTurnsPerPlayer; i++)
+            {
+                for (int j = 0; j < numberOfTurnsPerPlayer; j++)
+                {
+                    if ((Convert.ToInt32(field[i]) - 1) % 3 == (Convert.ToInt32(field[j]) - 1) % 3 && field[j] != null && field[i] != null)
+                    {
+                        numberOfIdenticalCharactera++;
+                        winnigCombination.Add(field[i]);
+                    }
+                }
+                if (maxNumberOfIdenticalCharactera < numberOfIdenticalCharactera)
+                {
+                    maxNumberOfIdenticalCharactera = numberOfIdenticalCharactera;
+                }
+                //if (winnigCombination.Count() != 3)
+                //{
+                //    winnigCombination.Clear();
+                //}
+                numberOfIdenticalCharactera = 0;
+            }
+            if (maxNumberOfIdenticalCharactera > 2)
+            {
+                return winnigCombination.ToArray();
+            }
+            // Проверка горизонтальных линий
+            maxNumberOfIdenticalCharactera = 0;
+            for(int i = 0; i < numberOfTurnsPerPlayer; i++)
+            {
+                for (int j = 0;j < numberOfTurnsPerPlayer; j++)
+                {
+                    if ((Convert.ToInt32(field[i]) - 1) / 3 == (Convert.ToInt32(field[j]) - 1) / 3 && field[j] != null && field[i] != null)
+                    {
+                        numberOfIdenticalCharactera++;
+                        winnigCombination.Add(field[i]);
+                    }
+                }
+                if (maxNumberOfIdenticalCharactera < numberOfIdenticalCharactera)
+                {
+                    maxNumberOfIdenticalCharactera = numberOfIdenticalCharactera;
+                }
+                //if (winnigCombination.Count() != 3)
+                //{
+                //    winnigCombination.Clear();
+                //}
+                numberOfIdenticalCharactera = 0;
+            }
+            if (maxNumberOfIdenticalCharactera > 2)
+            {
+                return winnigCombination.ToArray();
+            }
+            
+
+            return null;
+        }
+
+        static string NumberCheck()
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > 0 && number < 10)
+            {
+                return number.ToString();
+            }
+            return "Ошибка";
+        }
+        static void ShowWinningCombination(string[,] fieldOfPlay, string[] winnigCombination)
+        {
+            for(int i = 0; i < 3; i++) 
+            {
+                for(int j = 0; j < 3; j++) 
+                {
+                    if (СheckElementInArray(winnigCombination, i, j))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(fieldOfPlay[i, j]);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("|");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(fieldOfPlay[i, j]);
+                        Console.Write("|");
+                    }
+                }
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("------");
+            }
+
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 3; j++)
+            //    {
+            //        Console.Write(fieldOfPlay[i, j] + "|");
+            //    }
+            //    Console.WriteLine();
+            //    Console.WriteLine("------");
+            //}
+        }
+        static bool СheckElementInArray(string[] winnigCombination, int i, int j)
+        {
+            bool isElementInArray = false;
+            for (int k = 0; k < winnigCombination.Length; k++)
+            {
+                if (Convert.ToInt32(winnigCombination[k]) == i * 3 + j + 1)
+                {
+                    isElementInArray = true;
+                }
+            }
+            return isElementInArray;
+        }
+    }
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,154 @@
+﻿using System.Threading.Channels;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] fieldOfPlay = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+            bool isWin = false;
+            int moveCounter = 0;
+            string errMes = "";
+            while (moveCounter < 9)
+            {
+                Console.Clear();
+                for(int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Console.Write(fieldOfPlay[i, j] + "|");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("------");
+                }
+                if (errMes != "")
+                {
+                    Console.WriteLine(errMes);
+                }
+                errMes = "";
+                Console.Write(moveCounter % 2 == 0 ? "Ход игрока 1 (Крестик): " : "Ход игрока 2 (Нолик): ");
+                string playerTurn = Console.ReadLine();
+                if (int.TryParse(playerTurn, out int result))
+                {
+                    if (!(result > 0 && result < 10)) 
+                    {
+                        errMes = "Поробуйте ввести число от 1 до 9";
+                        continue;
+                    }
+                }
+                else
+                {
+                    errMes = "Поробуйте ввести число от 1 до 9";
+                    continue;
+                }
+                
+                if (fieldOfPlay[(Convert.ToInt32(playerTurn) - 1) / 3, (Convert.ToInt32(playerTurn) - 1) % 3] == "X" || 
+                    fieldOfPlay[(Convert.ToInt32(playerTurn) - 1) / 3, (Convert.ToInt32(playerTurn) - 1) % 3] == "O")
+                {
+                    errMes = "Данная ячейка уже заполнена, попробуйте другую." ;
+                    continue;
+                }
+                else
+                {
+                    fieldOfPlay[(Convert.ToInt32(playerTurn) - 1)/ 3, (Convert.ToInt32(playerTurn) - 1) % 3] = moveCounter % 2 == 0 ? "X" : "O";
+                }
+                moveCounter++;
+                string[] winningCombination = new string[3]; 
+                bool isGreen = false;
+                if (PlayerWinCheck(fieldOfPlay) != null && moveCounter < 10)
+                {
+                    
+                    Console.Clear();
+                    Console.WriteLine(moveCounter % 2 == 0 ? "Победа игрока 2 (Нолик): " : "Победа игрока 1 (Крестик): ");
+                    Console.WriteLine();
+                    winningCombination = PlayerWinCheck(fieldOfPlay);
+                    for (int i = 0;i < 3; i++)
+                    {
+                        for (int j = 0;j < 3; j++)
+                        {
+                            for (int k = 0;k < 3; k++)
+                            {
+                                if ((3 * i + j + 1).ToString() == winningCombination[k])
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.Write(fieldOfPlay[i, j]);
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.Write("|");
+                                    isGreen = true;
+                                }
+                            }
+                            if (!isGreen)
+                            {
+                                Console.Write(fieldOfPlay[i, j] + "|");
+                            }
+                            isGreen = false;
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("------");
+                    }
+                    isWin = true;
+                    break;
+                }
+            }
+            if (!isWin)
+            {
+                Console.Clear();
+                Console.WriteLine("Ничья");
+                Console.WriteLine();
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Console.Write(fieldOfPlay[i, j] + "|");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("------");
+                }
+            }
+
+        }
+        /// <summary>
+        /// Проверка, что игрок выиграл.
+        /// </summary>
+        /// <param name="fieldOfPlay"></param>
+        /// <returns>Возращает победную комбинацию полей.</returns>
+        static string[] PlayerWinCheck(string[,] fieldOfPlay)
+        {
+            string[] winningCombination = new string[3];    
+            for (int i = 0; i < 3; i++)
+            {
+                // проверка 
+                if (fieldOfPlay[0, i] == fieldOfPlay[1, i] && fieldOfPlay[1, i] == fieldOfPlay[2, i])
+                {
+                    winningCombination[0] = (3 * 0 + i + 1).ToString();
+                    winningCombination[1] = (3 * 1 + i + 1).ToString();
+                    winningCombination[2] = (3 * 2 + i + 1).ToString();
+                    return winningCombination;
+                }
+                // проверка горизонтальной линии
+                else if (fieldOfPlay[i, 0] == fieldOfPlay[i, 1] && fieldOfPlay[i, 1] == fieldOfPlay[i, 2]){
+                    winningCombination[0] = (3 * i + 0 + 1).ToString();
+                    winningCombination[1] = (3 * i + 1 + 1).ToString();
+                    winningCombination[2] = (3 * i + 2 + 1).ToString();
+                    return winningCombination;
+                }
+                else if (fieldOfPlay[0, 0] == fieldOfPlay[1, 1] && fieldOfPlay[0, 0] == fieldOfPlay[2, 2])
+                {
+                    winningCombination[0] = "1";
+                    winningCombination[1] = "5";
+                    winningCombination[2] = "9";
+                    return winningCombination;
+                }
+                else if (fieldOfPlay[2, 0] == fieldOfPlay[1, 1] && fieldOfPlay[2, 0] == fieldOfPlay[0, 2])
+                {
+                    winningCombination[0] = "3";
+                    winningCombination[1] = "5";
+                    winningCombination[2] = "7";
+                    return winningCombination;
+                }
+            }
+            return null;
+        }
+    }
+}
